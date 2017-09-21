d98d5d60c5e7efdfdd7cb680f7f16ba4beb45045
Claudio Alarcon <claudio_alarcon@mentor.com>
2017-09-21 16:15:28 -0300

    Preparing locale; excluding more patterns

--- pads_maker/conf.py
+++ pads_maker/conf.py
@@ -65,12 +65,21 @@ release = u'2.0'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+locale_dirs = ['../locale']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'schematics', 'pcb', 'crossprobing']
+exclude_patterns = [
+    'vias.rst',
+    'parts.rst',
+    'apps-header.rst',
+    'icons-logos.rst',
+    '_build',
+    'Thumbs.db',
+    'schematics',
+    'pcb',
+]
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'

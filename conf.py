9d80af78824100adaa81d66d04b7ed5f2515f0ab
Claudio Alarcon <claudio_alarcon@mentor.com>
2017-09-22 12:50:56 -0300

    Fixing locale location; gettext not compact

--- conf.py
+++ conf.py
@@ -82,7 +82,8 @@ else:
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-locale_dirs = ['../locale']
+locale_dirs = ['locale']
+gettext_compact = False
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
